bf3c418828cf42a89402b82cdae5557ad08f744b
carloncho155 <44416116+carloncho155@users.noreply.github.com>
2018-11-13 23:52:40 -0500

    Add files via upload

--- BusinessLogicLayer/UsuarioBL.vb
+++ BusinessLogicLayer/UsuarioBL.vb
@@ -7,4 +7,8 @@ Public Class UsuarioBL
         Return oPersonaDA.ClienteLoginValidar(usuario, password)
     End Function
 
+    Public Function RegistroUsuario(ByVal nombres As String, ByVal apePat As String, ByVal apeMat As String, ByVal correo As String, ByVal tipodoc As Integer, ByVal documento As String, ByVal fecNac As String, ByVal direccion As String, ByVal telefono As String, ByVal usuario As String, ByVal password As String) As Integer
+        Return oPersonaDA.RegistrarUsuario(nombres, apePat, apeMat, correo, tipodoc, documento, fecNac, direccion, telefono, usuario, password)
+    End Function
+
 End Class
